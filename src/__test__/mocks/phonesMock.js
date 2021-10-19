59f97ccd37faf70b36eefd5ce37cd8a751388790
elenamlopez <elena.mateos1@gmail.com>
2021-10-19 12:24:49 +0200

    Add test

---
+++ src/__test__/mocks/phonesMock.js
@@ -0,0 +1,39 @@
+export const phonesMock = [
+  {
+      "id": 0,
+      "name": "iPony 007",
+      "manufacturer": "Banana",
+      "description": "Poopayee bananaaaa po kass baboiii para tú. Bee do bee do bee do hahaha la bodaaa daa jeje. Po kass wiiiii belloo! Tatata bala tu para tú. Bappleees para tú daa poopayee ti aamoo!",
+      "color": "black",
+      "price": 769,
+      "imageFileName": "IPhone_7.png",
+      "screen": "4,7 inch IPS",
+      "processor": "A10 Fusion",
+      "ram": 2
+  },
+  {
+      "id": 1,
+      "name": "BlackHole 8000",
+      "manufacturer": "Supermassive",
+      "description": "Minions ipsum para tú hana dul sae potatoooo baboiii gelatooo belloo! Underweaaar poopayee bappleees bee do bee do bee do. Chasy hahaha aaaaaah la bodaaa underweaaar bee do bee do bee do jiji belloo! Belloo! Uuuhhh. Uuuhhh hana dul sae me want bananaaa! ",
+      "color": "grey",
+      "price": 209,
+      "imageFileName": "Galaxy_S7.png",
+      "screen": "5,1 inch Quad-HD",
+      "processor": "Snapdragon 820",
+      "ram": 4
+  }
+]
+
+export const phoneMock = {
+    "id": 0,
+    "name": "iPony 007",
+    "manufacturer": "Banana",
+    "description": "Poopayee bananaaaa po kass baboiii para tú. Bee do bee do bee do hahaha la bodaaa daa jeje. Po kass wiiiii belloo! Tatata bala tu para tú. Bappleees para tú daa poopayee ti aamoo!",
+    "color": "black",
+    "price": 769,
+    "imageFileName": "IPhone_7.png",
+    "screen": "4,7 inch IPS",
+    "processor": "A10 Fusion",
+    "ram": 2
+}
