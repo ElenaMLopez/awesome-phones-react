c0dd7aa31245709a26e2c328361b387be4098c7e
elenamlopez <elena.mateos1@gmail.com>
2021-10-20 13:18:52 +0200

    Add detail container tests

--- src/__test__/PhoneDetailComponent.test.js
+++ src/__test__/PhoneDetailComponent.test.js
@@ -4,10 +4,24 @@ import { phoneMock } from './mocks/phonesMock';
 import { MemoryRouter } from 'react-router-dom';
 
 test('Render a phone detail page', async () => {
-
   render(<PhoneDetailComponent phone={phoneMock} />, {wrapper: MemoryRouter});
 
   const phoneName = await screen.findByText(/iPony 007/i)
 
   expect(phoneName).toBeInTheDocument();
 })
+
+test('There is a link to navigate to Home view', async () => {
+  render(<PhoneDetailComponent phone={phoneMock} />, {wrapper: MemoryRouter});
+  const homeLink = await screen.findByText(/Home/i);
+
+  expect(homeLink).toBeInTheDocument();
+})
+
+test('There is a button to delete the phone', async () => {
+  render(<PhoneDetailComponent phone={phoneMock} />, {wrapper: MemoryRouter});
+  const deleteButton = await screen.findByText(/Delete/i);
+
+  expect(deleteButton).toBeInTheDocument();
+})
+
--- src/__test__/PhoneDetailContainer.test.js
+++ src/__test__/PhoneDetailContainer.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { waitFor, render, screen } from '@testing-library/react';
+import user from "@testing-library/user-event";
+import Snackbar from '../components/commons/Snackbar';
 import PhoneDetailContainer from '../components/PhoneDetail/PhoneDetailContainer';
 import { phoneMock } from './mocks/phonesMock';
 import { MemoryRouter } from 'react-router-dom';
@@ -24,4 +26,20 @@ test('Send description to detail component', async () => {
 
   const phoneDescription = await screen.findByText(/Poopayee bananaaaa/i);
   expect(phoneDescription).toBeInTheDocument();
+})
+
+
+test('It shows a message when user delete a phone', async () => {
+  useSelectorMock.mockReturnValue(phoneMock);
+  useDispatchMock.mockReturnValue(dispatch);
+
+  render(<PhoneDetailContainer location={locationMock} />, {wrapper: MemoryRouter})
+  const {getByText} = render(<Snackbar />)
+
+  const deleteButton = await screen.findByText(/Delete/i);
+  user.click(deleteButton);
+
+  await waitFor(() => {
+    expect(getByText(/You have deleted this phone/i)).toBeInTheDocument()
+  })
 })
